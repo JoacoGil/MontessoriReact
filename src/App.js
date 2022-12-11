fc4117b336922524c7fef22c0d3742c8d1757f68
JoacoGil <joaquingil2005@gmail.com>
2022-12-11 15:38:13 -0300

    Scroll to top

--- src/App.js
+++ src/App.js
@@ -8,23 +8,25 @@ import Inicio from './pages/Inicio';
 import Vermas from './pages/Vermas';
 import { Footer } from './components/Footer/Footer';
 import Contacto from './pages/Contacto';
+import ScrollToTop from './components/ScrollToTop';
 
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <div className='contenedor-principal'>
-        <Routes>
-          <Route exact path="/" element={<Inicio />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Vermas" element={<Vermas />} />
-          <Route path="/Contacto" element={<Contacto />} />
-        </Routes>
+      <ScrollToTop />
+        <div className="App">
+          <Header />
+          <div className='contenedor-principal'>
+          <Routes>
+            <Route exact path="/" element={<Inicio />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Vermas" element={<Vermas />} />
+            <Route path="/Contacto" element={<Contacto />} />
+          </Routes>
+          </div>
         </div>
-      </div>
-      <Footer/>
+        <Footer/>
     </Router>
   );
 }
